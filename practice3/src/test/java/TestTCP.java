6b91225d52ffa0491d9ae0fab90a39c95dfeca3c
yehorPo <66732525+yehorPo@users.noreply.github.com>
2021-06-05 17:13:51 +0300

    add TCP test

---
+++ practice3/src/test/java/TestTCP.java
@@ -0,0 +1,48 @@
+import org.junit.Test;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import java.io.IOException;
+
+public class TestTCP {
+    @Test
+    public void test1(){
+        ServerTCP server = null;
+        try {
+            server = new ServerTCP(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        server.start();
+        System.out.println("==Server started==");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Packet packet = null;
+        try {
+            packet  = new Packet((byte) 1, 1L, new Message(Message.cTypes.ok,1,"sender"));
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        System.out.println("==Packet inited==");
+        SenderTCP sender = new SenderTCP(packet);
+        sender.start();
+        System.out.println("==Sender started==");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        server.shutdown();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("==Server closed==");
+    }
+}
